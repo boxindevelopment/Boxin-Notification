cbaf41bc21cdc424c395fe4fa9d9cd7f7c5da739
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2018-12-06 22:55:46 +0700

    api with middleware auth passport

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Http\Request;
 
 Route::group(['namespace' => 'Api'], function () {
     
-    Route::group(['middleware' => 'jwt.auth'], function () {
+	Route::prefix('user')->middleware('auth:api')->group(function() {
         Route::post('/device-token', ['uses' => 'UsersController@deviceToken', 'as' => 'api.user.postToken']);
         Route::delete('/device-token', ['uses' => 'UsersController@deviceTokenDelete', 'as' => 'api.user.deleteToken']);
     });
