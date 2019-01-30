80d755cf150a1bd23d9f171645014441b124507d
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-01-30 14:12:56 +0700

    List notifications

---
+++ app/Http/Controllers/Api/NotificationController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Resources\NotificationResource;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Notification;
+use Validator;
+
+class NotificationController extends Controller
+{
+
+    /**
+     * @var
+     */
+
+    public function __construct()
+    {
+    }
+
+    public function index(Request $request)
+    {
+        $users = $request->user();
+
+        try {
+
+            $notification = Notification::where('user_id', $users->id)->paginate(15);
+            // return response()->json($notification);
+            return NotificationResource::collection($notification);
+
+        } catch (ValidatorException $e) {
+            return response()->json($e);
+        }
+
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,7 @@ Route::group(['namespace' => 'Api'], function () {
     Route::prefix('user')->middleware('auth:api')->group(function() {
         Route::post('/device-token', ['uses' => 'UserDeviceController@deviceToken', 'as' => 'api.user_device.postToken']);
         Route::delete('/device-token', ['uses' => 'UserDeviceController@deviceTokenDelete', 'as' => 'api.user_device.deleteToken']);
+        Route::get('/notifications', ['uses' => 'NotificationController@index', 'as' => 'api.notification.index']);
     });
 
     //1/2 Admin PaymentController function update status = 7 (Approved), 8 = (Rejected)
@@ -36,6 +37,6 @@ Route::group(['namespace' => 'Api'], function () {
     Route::post('/pickup/stored/{user_id}', ['uses' => 'NotificationPickupStoredController@pickupStored', 'as' => 'api.notification.pickup.stored']);
     Route::post('/return-request/{user_id}', ['uses' => 'NotificationReturnRequestController@returnRequest', 'as' => 'api.notification.return.request']);
     //9. Admin ReturnBoxesController function update status = 12
-    Route::post('/returned/{user_id}', ['uses' => 'NotificationReturnedController@returned', 'as' => 'api.notification.returned']);
+        Route::post('/returned/{user_id}', ['uses' => 'NotificationReturnedController@returned', 'as' => 'api.notification.returned']);
 
 });
