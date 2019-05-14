b37092ef16825033f60d980c677eb5fc55785023
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2019-05-14 11:52:49 +0700

    Notification Box last day of storage

---
+++ app/Console/Commands/ExpiredBox.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Http\Resources\OrderDetailResource;
+use App\Jobs\Notif\ItemStored;
+use App\Models\OrderDetailBox;
+use App\Models\Notification;
+use App\Models\OrderDetail;
+use Carbon\Carbon;
+use DB;
+use Log;
+
+use Illuminate\Console\Command;
+
+class ExpiredBox extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'Notif:ExpiredBox';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Notification Expired Box';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+
+
+        $now = Carbon::now();
+        $beforeDay = $now->addDays('-1')->format('Y-m-d');
+        $now = Carbon::now();
+        $afterDay = $now->addDays('1');
+        $timeNow = Carbon::now('Asia/Jakarta')->format('H');
+
+        if((int)$timeNow == 11) {
+            $query          = OrderDetailBox::query();
+                              $query->where('status_id', 9);
+                              $query->whereRaw("[id] NOT IN (SELECT [notifiable_id] FROM [notifications] WHERE [notifiable_type] = 'OrderDetailBox')");
+                              $query->whereHas('order_detail', function($q) use ($beforeDay) {
+                                  $q->whereDate('end_date', $beforeDay);
+                              });
+            $orderDetailBox = $query->get();
+
+            if(count($orderDetailBox) > 0){
+                foreach ($orderDetailBox as $k => $v) {
+                    if($v->order_detail){
+                        $notification = Notification::where('notifiable_type', 'OrderDetailBox')
+                                                    ->where('notifiable_id', $v->id)
+                                                    ->get();
+                		$orderDetails     =  OrderDetail::select('order_details.*', DB::raw('orders.status_id as status_id'), DB::raw('orders.user_id as user_id'), DB::raw('DATEDIFF(day, order_details.start_date, order_details.end_date) as total_time'), DB::raw('DATEDIFF(day, order_details.start_date, GETDATE()) as selisih'))
+                						            ->leftJoin('orders', 'orders.id', '=', 'order_details.order_id')
+                						            ->where('order_details.id', $v->order_detail->id)
+                						            ->get();
+                        $data             = OrderDetailResource::collection($orderDetails);
+                		$title            = "Tomorrow is the last day of your storage";
+                        $token            = ItemStored::dispatch($v->order_detail->order->user_id, $title, $data, $v)->onQueue('processing');
+                    }
+                }
+
+            }
+        }
+
+    }
+}
--- app/Jobs/Notif/ItemStored.php
+++ app/Jobs/Notif/ItemStored.php
@@ -4,6 +4,7 @@ namespace App\Jobs\Notif;
 
 use App\Models\Notification;
 use App\Models\UserDevice;
+use App\Models\OrderDetailBox;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -18,11 +19,15 @@ class ItemStored implements ShouldQueue
 
     protected $user_id;
     protected $title;
+    protected $data;
+    protected $orderDetailBox;
 
-    public function __construct($user_id, $title)
+    public function __construct($user_id, $title, $data, OrderDetailBox $orderDetailBox)
     {
-        $this->user_id = $user_id;
-        $this->title = $title;
+        $this->user_id          = $user_id;
+        $this->title            = $title;
+        $this->data             = $data;
+        $this->orderDetailBox   = $orderDetailBox;
     }
 
     /**
@@ -34,24 +39,29 @@ class ItemStored implements ShouldQueue
     {
 
             $userDevices = UserDevice::where('user_id', $this->user_id)->get();
-            $params = [];
-            $params['include_player_ids'] = $userDevices->pluck('token');//array($userId);
-            $params['contents'] = ["en" => $this->title];
-            $params['headings'] = ["en" => $this->title];
-            $params['data'] = json_decode(json_encode(['type' => 'item-stored','detail' => ['message' => $this->title] ]));
-            OneSignal::sendNotificationCustom($params);
+            if($userDevices){
+                $params = [];
+                $params['include_player_ids'] = $userDevices->pluck('token');//array($userId);
+                if($params['include_player_ids']){
+                    $params['contents'] = ["en" => $this->title];
+                    $params['headings'] = ["en" => $this->title];
+                    $params['data'] = json_decode(json_encode(['type' => 'item-stored','detail' => ['message' => $this->title, 'data' => $this->data] ]));
+                    OneSignal::sendNotificationCustom($params);
 
-            $dataNotif['type'] = 'item stored';
-            $dataNotif['title'] = $this->title;
-            $dataNotif['user_id'] = $this->user_id;
-            $dataNotif['notifiable_type'] = 'user';
-            $dataNotif['notifiable_id'] = $this->user_id;
-            $dataNotif['data'] = json_encode(['type' => 'user','detail' => ['message' => $this->title] ]);
-            Notification::create($dataNotif);
-            \Log::info('Send Notif success');
-            \Log::info('Token:' . $userDevices->pluck('token'));
+                    $dataNotif['type'] = 'item stored';
+                    $dataNotif['title'] = $this->title;
+                    $dataNotif['user_id'] = $this->user_id;
+                    $dataNotif['order_id'] = $this->data[0]->order->id;
+                    $dataNotif['notifiable_type'] = 'OrderDetailBox';
+                    $dataNotif['notifiable_id'] = $this->orderDetailBox->id;
+                    $dataNotif['data'] = json_encode(['type' => 'user','detail' => ['message' => $this->title, 'data' => $this->data] ]);
+                    Notification::create($dataNotif);
+                    \Log::info('Send Notif success');
+                    \Log::info('Token:' . $userDevices->pluck('token'));
 
-            return $userDevices->pluck('token');
+                    return $userDevices->pluck('token');
+                }
+            }
 
     }
 }
