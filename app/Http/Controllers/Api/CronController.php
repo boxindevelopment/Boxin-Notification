9a08a1c1006a4d2e14a85d7fe51f84b679139520
Bahtiar Rivai <bahtiar2008.uchiha@gmail.com>
2020-03-03 11:35:25 +0700

    Cron Cancelled payment order

--- app/Http/Controllers/Api/CronController.php
+++ app/Http/Controllers/Api/CronController.php
@@ -2,17 +2,18 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Jobs\Notif\ConfirmPayment;
 use App\Http\Controllers\Controller;
-use App\Jobs\Contact\StoreContactJob;
+use App\Http\Resources\OrderDetailResource;
 use App\Models\Order;
 use App\Models\OrderDetail;
 use App\Models\PickupOrder;
 use App\Models\Box;
 use App\Models\SpaceSmall;
-use DB;
 use Carbon\Carbon;
-use Log;
+use DB;
 use Illuminate\Http\Request;
+use Log;
 
 class CronController extends Controller
 {
@@ -35,27 +36,57 @@ class CronController extends Controller
         $query->where('status_id', 14);
         $query->where('created_at', '<', $beforeDay);
         $orders = $query->get();
-        if(count($orders) > 0){
-            foreach ($orders as $k => $v) {
-                Log::info('id order : ' . $v->id);
-                foreach ($v->order_detail as $key => $d) {
-                    if($d->types_of_box_room_id == 1){
-                        $box = BOX::where('id', $d->room_or_box_id)->first();
-                        // $orders[$k]->order_detail[$key]->box = $box;
-                    } else if($d->types_of_box_room_id == 2){
-                        $space = SpaceSmall::where('id', $d->room_or_box_id)->first();
-                        // $orders[$k]->order_detail[$key]->space = $space;
+        DB::beginTransaction();
+        try {
+            if(count($orders) > 0){
+                foreach ($orders as $k => $v) {
+                    //Status cancelled orders
+                    $v->status_id = 24;
+                    $v->save();
+
+                    //Status cancelled order pickup
+                    $v->pickup_order->status_id = 24;
+                    $v->pickup_order->save();
+
+                    foreach ($v->order_detail as $key => $d) {
+                        //Status cancelled order detail
+                        $d->status_id = 24;
+                        $d->save();
+
+                        if($d->types_of_box_room_id == 1){
+                            //Status empty box
+                            Box::where('id',  $d->room_or_box_id)->update(['status_id' => 10]);
+                        } else if($d->types_of_box_room_id == 2){
+                            //Status empty space
+                            SpaceSmall::where('id', $d->room_or_box_id)->update(['status_id' => 10]);
+                        }
+
+                        Log::info('code : ' . $d->id_name);
+
                     }
-                    Log::info('code : ' . $d->id_name);
-                    // Box::where('id', $room_or_box_id)->update(['status_id' => 10]);
-                    // SpaceSmall::where('id', $room_or_box_id)->update(['status_id' => 10]);
+                    if(count( $v->order_detail) > 0){
+                        $status = 'rejected';
+                        $orderDetails =  OrderDetail::select('order_details.*', DB::raw('orders.status_id as status_id'), DB::raw('orders.user_id as user_id'), DB::raw('DATEDIFF(day, order_details.start_date, order_details.end_date) as total_time'), DB::raw('DATEDIFF(day, order_details.start_date, GETDATE()) as selisih'))
+                                                    ->leftJoin('orders', 'orders.id', '=', 'order_details.order_id')
+                                                    ->where('order_details.id', $v->order_detail[0]->id)
+                                                    ->get();
+                        if(count($orderDetails) > 0) {
+                            $data = OrderDetailResource::collection($orderDetails);
+                            $confirm = ConfirmPayment::dispatch($v->user_id, $status, $data)->onQueue('processing');
+                        }
+                    }
                 }
+                return response()->json(['status' => 'success', 'message' => 'Order Count : ' . count($orders), 'order' => $orders], 200);
+            } else {
+                Log::info('No Order');
+                return response()->json(['status' => 'error', 'message' => 'No Order'], 402);
             }
-            return response()->json(['status' => 'success', 'message' => 'Order Count : ' . count($orders), 'order' => $orders], 200);
-        } else {
-            Log::info('No Order');
-            return response()->json(['status' => 'error', 'message' => 'No Order'], 402);
+        } catch (Exception $e) {
+            DB::rollback();
+            return response()->json([
+                'status' =>false,
+                'message' => $e->getMessage()
+            ], 401);
         }
-        return true;
     }
 }
