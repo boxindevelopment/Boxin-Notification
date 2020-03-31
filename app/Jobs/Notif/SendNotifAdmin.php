2ec1e3d0c4362f8007fb6ef046a52ea4ffe68480
bahtiar <bahtiar2008.uchiha@gmail.com>
2020-03-31 23:59:24 +0700

    admin notif

---
+++ app/Jobs/Notif/SendNotifAdmin.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace App\Jobs\Notif;
+
+use App\Models\Notification;
+use App\Models\UserDevice;
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use OneSignal;
+
+class SendNotifAdmin implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    protected $user_id;
+    protected $title;
+    protected $data;
+    protected $type;
+    protected $types;
+
+    public function __construct($user_id, $title, $data, $type, $types)
+    {
+        $this->user_id = $user_id;
+        $this->title = $title;
+        $this->data = $data;
+        $this->type = $type;
+        $this->types = $types;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+
+        $userDevices = UserDevice::where('user_id', $this->user_id)->get();
+        $token = $userDevices->pluck('token');
+        $token = UserDevice::where('device', 'web')->get()->pluck('token');
+        if($token){
+            $params = [];
+            $params['include_player_ids'] = $token;
+            $params['contents'] = ["en" => $this->title];
+            $params['headings'] = ["en" => $this->title];
+            $params['data'] = json_decode(json_encode(['type' => $this->type,'detail' => ['message' => $this->title, 'data' => $this->data] ]));
+            OneSignal::sendNotificationCustom($params);
+
+            $admins = User::where('roles_id', 3)->get();
+            if($admins){
+                foreach($admins as $kAdmin => $vAdmin){
+                    $dataNotifAdmin['type'] = $this->types;
+                    $dataNotifAdmin['title'] = $this->title;
+                    $dataNotifAdmin['user_id'] = $vAdmin->id;
+                    $dataNotifAdmin['send_user'] = $this->user_id;
+                    if($this->data){
+                        if($this->data[0]){
+                            $dataNotifAdmin['order_id'] = $this->data[0]->order->id;
+                        } else {
+                            $dataNotifAdmin['order_id'] = $this->data->order->id;
+                        }
+                    }
+                    $dataNotifAdmin['notifiable_type'] = 'admin';
+                    $dataNotifAdmin['notifiable_id'] = $vAdmin->id;
+                    $dataNotifAdmin['data'] = json_encode(['type' => 'admin','detail' => ['message' => $this->title, 'data' => $this->data] ]);
+                    $notification = Notification::create($dataNotifAdmin);
+                }
+            }
+
+            return $token;
+        } else {
+            return false;
+        }
+
+    }
+}
