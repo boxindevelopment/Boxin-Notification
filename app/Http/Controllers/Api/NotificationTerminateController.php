4fff2d24786152089ce819bf1a0fe3c5828f0be9
bahtiar <bahtiar2008.uchiha@gmail.com>
2020-04-01 09:56:47 +0700

    Terminate request with data

--- app/Http/Controllers/Api/NotificationTerminateController.php
+++ app/Http/Controllers/Api/NotificationTerminateController.php
@@ -3,9 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Http\Resources\OrderDetailResource;
 use App\Jobs\Notif\SendNotifAdmin;
-use App\Models\OrderDetail;
+use App\Models\ReturnBoxes;
 use DB;
 use Illuminate\Http\Request;
 
@@ -21,7 +20,7 @@ class NotificationTerminateController extends Controller {
 	 * @param  \Illuminate\Http\Request  $request
 	 * @return \Illuminate\Http\Response
 	 */
-	public function terminate(Request $request, $user_id)
+	public function terminate(Request $request, $terminate_id)
 	{
 
         $validator = \Validator::make($request->all(), [
@@ -34,17 +33,16 @@ class NotificationTerminateController extends Controller {
                 'message' => $validator->errors()
             ]);
         }
-		$orderDetails =  OrderDetail::select('order_details.*', 'users.first_name', 'users.last_name', DB::raw('orders.status_id as status_id'), DB::raw('orders.user_id as user_id'), DB::raw('DATEDIFF(day, order_details.start_date, order_details.end_date) as total_time'), DB::raw('DATEDIFF(day, order_details.start_date, GETDATE()) as selisih'))
-                                    ->leftJoin('orders', 'orders.id', '=', 'order_details.order_id')
-                                    ->leftJoin('users', 'users.id', '=', 'orders.user_id')
-						            ->where('order_details.id', $request->order_detail_id)
-						            ->get();
+		$terminate =  ReturnBoxes::select('return_boxes.*', 'order_details.id_name', 'users.first_name', 'users.last_name', DB::raw('orders.status_id as status_order_id'), DB::raw('orders.user_id as user_id'))
+									->leftJoin('order_details', 'order_details.id', '=', 'return_boxes.order_detail_id')
+									->leftJoin('orders', 'orders.id', '=', 'order_details.order_id')
+									->leftJoin('users', 'users.id', '=', 'orders.user_id')
+									->where('return_boxes.id', $terminate_id)
+									->first();
 
-		if(count($orderDetails) > 0) {
-
-			$data = OrderDetailResource::collection($orderDetails);
-			$title = "user " . $orderDetails[0]->first_name . " " . $orderDetails[0]->last_name . ", terminate request no order " . $orderDetails[0]->id_name;
-	        SendNotifAdmin::dispatch($user_id, $title, $data, 'terminate-request', 'terminate request')->onQueue('processing');
+		if($terminate) {
+			$title = "user " . $terminate->first_name . " " . $terminate->last_name . ", terminate request no order " . $terminate->id_name;
+	        SendNotifAdmin::dispatch($terminate_id, $title, $terminate, 'terminate-request', 'terminate request')->onQueue('processing');
 			return response()->json(['status' => 'success', 'message' => $title], 200);
 		}
 	}
